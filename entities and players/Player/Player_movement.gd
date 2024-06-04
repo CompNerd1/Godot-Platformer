864ac048d26786d79a1e63fc2204010ee5a8c773
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-06-04 12:59:28 -0700

    started adding run, changed movement

--- entities and players/Player/Player_movement.gd
+++ entities and players/Player/Player_movement.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var jump_height = 1
+const SPEED_CONST = 3.0
+const SLOW_DOWN = 300.0
+const JUMP_VELOCITY = -4.0
+
+@export var speed: int
+@export var run_speed: int
+@export var jump_height: int
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,14 +20,18 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * jump_height
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	var run = false
+	if run:
+		var run = Input.is_action_just_pressed("Run")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED_CONST * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SLOW_DOWN)
 
 	move_and_slide()
