14b643d547672789a6541b28a576e0255afdba33
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-06-18 12:58:29 -0700

    tried adding camera movement, not updating every frame

--- entities and players/Player/Camera_Movement.gd
+++ entities and players/Player/Camera_Movement.gd
@@ -1,10 +1,8 @@
 extends Camera2D
 
+@onready var player = $"../Player"
+@export var speed = 5
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	position.x = lerp(position.x,player.position.x,speed*delta)
+	position.y = lerp(position.y,player.position.y,speed*delta)
