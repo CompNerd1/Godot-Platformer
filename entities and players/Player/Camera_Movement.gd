59bd16d5ee3da1eef1b77f5c356a5a9fe1a627b0
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-09-23 13:05:40 -0700

    CAMERA WORKS!

--- entities and players/Player/Camera_Movement.gd
+++ entities and players/Player/Camera_Movement.gd
@@ -1,8 +1,9 @@
 extends Camera2D
 
-@onready var player = get_node("/root/Level1/Player")
-@export var speed = 5
+@onready var player = get_node("../Player/CharacterBody2D")
+@onready var player_add = get_node("../Player")
+@export var speed = 7
 
 func _physics_process(delta):
-	position.x = lerp(position.x,player.position.x,speed*delta)
-	position.y = lerp(position.y,player.position.y,speed*delta)
+	position.x = lerp(position.x,player.position.x+player_add.position.x,speed*delta)
+	position.y = 324
