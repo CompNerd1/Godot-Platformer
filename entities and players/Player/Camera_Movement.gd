bbf6a456fe46fe7fabb5297f75ac76ed1bb9b4e1
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-09-24 13:21:13 -0700

    '?'

--- entities and players/Player/Camera_Movement.gd
+++ entities and players/Player/Camera_Movement.gd
@@ -3,7 +3,11 @@ extends Camera2D
 @onready var player = get_node("../Player/CharacterBody2D")
 @onready var player_add = get_node("../Player")
 @export var speed = 7
+var screen_size = 648
+var y_level
+var location
 
 func _physics_process(delta):
+	location = player.position.y + player_add.position.y
 	position.x = lerp(position.x,player.position.x+player_add.position.x,speed*delta)
-	position.y = 324
+	position.y = screen_size / 2
