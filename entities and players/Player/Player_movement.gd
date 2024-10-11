eaaf096af59fe0f54ae3aca4ce2b70215027f199
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-10-11 10:56:39 -0700

    movement fixed

--- entities and players/Player/Player_movement.gd
+++ entities and players/Player/Player_movement.gd
@@ -1,9 +1,8 @@
 extends CharacterBody2D
 
-const JUMP_VELOCITY = -4.0
+const JUMP_VELOCITY = -4
 
 @export var acceleration: int
-@export var run_acceleration: int
 @export var max_speed: int
 @export var max_run_speed: int
 @export var jump_speed: int
@@ -16,21 +15,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var knockback = false
 var run = false
 
-var timer = Timer.new()
+
 
 func _ready() -> void:
 	Globals.health = health
 
+
 func on_hit():
-	velocity.x = -1 * (velocity.x / abs(velocity.x)) * 500
-	velocity.y = JUMP_VELOCITY * jump_speed * 0.
+	if velocity.x != 0:
+		velocity.x = -1 * (velocity.x / abs(velocity.x)) * 500
+	velocity.y = JUMP_VELOCITY * jump_speed * 0.75
 	knockback = true
-	timer.start(1.5)
+	$Timer.start()
 
 func _on_timer_timeout() -> void:
 	knockback = false
 
 func _physics_process(delta):
+	#RUN
+	if Input.is_action_just_pressed("Run"):
+		run = !run
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -45,28 +50,44 @@ func _physics_process(delta):
 	
 	#movement
 	if not knockback:
-		if Input.is_action_pressed("ui_right"):
-			velocity.x += acceleration
-			if velocity.x > max_speed:
-				velocity.x = max_speed
-		if Input.is_action_pressed("ui_left"):
-			velocity.x -= acceleration
-			if abs(velocity.x) > max_speed:
-				velocity.x = -1 * max_speed
-			
+		if !run:
+			if Input.is_action_pressed("ui_right"):
+				velocity.x += acceleration / 2
+				if velocity.x > max_speed:
+					velocity.x = max_speed
+			if Input.is_action_pressed("ui_left"):
+				velocity.x -= acceleration / 2
+				if abs(velocity.x) > max_speed:
+					velocity.x = -1 * max_speed
+		else:
+			if Input.is_action_pressed("ui_right"):
+				velocity.x += acceleration
+			if velocity.x > max_run_speed:
+				velocity.x = max_run_speed
+			if Input.is_action_pressed("ui_left"):
+				velocity.x -= acceleration
+				if abs(velocity.x) > max_run_speed:
+					velocity.x = -1 * max_run_speed
+	#friction
 	if not Input.is_action_pressed("ui_right"):
 		if not Input.is_action_pressed("ui_left"):
 			if abs(velocity.x) < friction:
 				velocity.x = 0
 			if is_on_floor():
-				if velocity.x > 0:
-					velocity.x -= friction
-				if velocity.x < 0:
-					velocity.x += friction
+				if !run:
+					if velocity.x > 0:
+						velocity.x -= friction
+					if velocity.x < 0:
+						velocity.x += friction
+				else:
+					if velocity.x > 0:
+						velocity.x -= friction * 1.5
+					if velocity.x < 0:
+						velocity.x += friction * 1.5
 			else:
 				if velocity.x > 0:
 					velocity.x -= friction / 1.25
 				if velocity.x < 0:
 					velocity.x += friction / 1.25
-	
+	print(position)
 	move_and_slide()
