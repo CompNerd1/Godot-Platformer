d33ec351c87cfb6ba301a82399ba48c37078f647
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-11-04 09:38:23 -0800

    pickup

--- Items/Pickup/Pickup.gd
+++ Items/Pickup/Pickup.gd
@@ -1,6 +1,12 @@
-extends CollisionPolygon2D
+extends Area2D
 
-func _on_pickup_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("player")):
 		Globals.power_up = true
-		queue_free()
+		$Sprite2D.visible = false
+		$Timer.start()
+		collision_mask = 0
+
+func _on_timer_timeout() -> void:
+	collision_mask = 1
+	$Sprite2D.visible = true
--- entities and players/Player/Camera_Movement.gd
+++ entities and players/Player/Camera_Movement.gd
@@ -1,16 +1,15 @@
 extends Camera2D
 
-@onready var player = get_node("../Player/CharacterBody2D")
-@onready var player_add = get_node("../Player")
+@onready var player = get_node("../Player")
 @export var speed = 7
-var screen_size = 648
+var screen_size = 650
 var y_level
 var location
 
 func _physics_process(delta):
-	location = player.position.y + player_add.position.y
+	location = player.position.y
 	y_level = abs(location) as int / screen_size
-	position.x = lerp(position.x,player.position.x+player_add.position.x,speed*delta)
+	position.x = lerp(position.x,player.position.x,speed*delta)
 	if(location > 0):
 		position.y = lerp(position.y, screen_size as float / 2, speed * 2 * delta)
 	else:
