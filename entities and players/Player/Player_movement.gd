624e5a7501a1968dfa21fd99f7959a36978e43fc
Buckiryv000 <Buckiryv000@edmonds15.org>
2024-11-14 13:49:05 -0800

    added climb, and made running 'animation'

--- entities and players/Player/Player_movement.gd
+++ entities and players/Player/Player_movement.gd
@@ -9,6 +9,7 @@ const JUMP_VELOCITY = -1
 @export var health: int
 @export var friction: float
 @export var terminal_velocity: int
+@export var climb_velocity: int
 var jumptime = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,16 +20,21 @@ var run = false
 
 func _process(delta: float) -> void:
 	if velocity.y >= terminal_velocity * 0.75 and jumptime:
-		$CharSprite.set_frame(3);
+		$CharSprite.set_frame(3)
 	if velocity.y < terminal_velocity * 0.75 and velocity.y >= terminal_velocity * 0.25:
-		$CharSprite.set_frame(1);
+		$CharSprite.set_frame(1)
 	if velocity.y < terminal_velocity * 0.25:
-		$CharSprite.set_frame(14);
+		$CharSprite.set_frame(14)
 	if velocity.y == 0:
-		$CharSprite.set_frame(5);
+		$CharSprite.set_frame(5)
+	if run and velocity.x != 0:
+		$CharSprite.set_frame(6)
+	else:
+		$CharSprite.set_frame(5)
 
 func _ready() -> void:
 	Globals.health = health
+	climb_velocity *= -1
 
 func enemy_on_hit(x_velocity: float):
 	if velocity.x == 0:
@@ -46,6 +52,15 @@ func _on_timer_timeout() -> void:
 	knockback = false
 
 func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_left"):
+		$CharSprite.scale = Vector2(-1,1)
+	if Input.is_action_just_pressed("ui_right"):
+		$CharSprite.scale = Vector2(1,1)
+	
+	#climb
+	if Globals.climb and Input.is_action_pressed("Jump"):
+		velocity.y = climb_velocity
+	
 	#RUN
 	if Input.is_action_just_pressed("Run"):
 		run = !run
@@ -62,12 +77,12 @@ func _physics_process(delta):
 			velocity.y += gravity
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY * jump_speed
 		$JumpTime.start();
 	
 	#Double jump
-	if Input.is_action_just_pressed("ui_accept") and !is_on_floor() and Globals.power_up:
+	if Input.is_action_just_pressed("Jump") and !is_on_floor() and Globals.power_up:
 		velocity.y = JUMP_VELOCITY * jump_speed
 		Globals.power_up = false
 		$JumpTime.start();
@@ -75,7 +90,7 @@ func _physics_process(delta):
 	#movement
 	if not knockback:
 		#run
-		if not run or (Globals.climb and not is_on_floor()):
+		if not run:
 			if Input.is_action_pressed("ui_right"):
 				velocity.x += acceleration / 2
 				if velocity.x > max_speed:
@@ -94,6 +109,7 @@ func _physics_process(delta):
 				velocity.x -= acceleration
 				if abs(velocity.x) > max_run_speed:
 					velocity.x = -1 * max_run_speed
+	
 	#friction
 	if not Input.is_action_pressed("ui_right"):
 		if not Input.is_action_pressed("ui_left"):
@@ -133,3 +149,4 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 #check if you are leave a physics climb (physics layer 2) tile
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	Globals.climb = false
+	velocity.y = 0
